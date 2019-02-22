6b89ef28c05f49d29638289999a8eed208a3a486
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-22 18:50:13 -0500

    changed paths

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
+$s3 = aws/aws-sdk-php/src/S3/S3Client.php::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
     <head><meta charset="UTF-8"></head>
     <body>
         <h1>Hello SOFE4630</h1>
-		
-		<a href="https://floating-beyond-79601.herokuapp.com/list.php">Files List</a>
+
+		<a href="https://sofe4630a2kdc.herokuapp.com//list.php">Files List</a>
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo

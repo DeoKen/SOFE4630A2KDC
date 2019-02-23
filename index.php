aecb8684863f56d36b6352f1058ecdf96da85bec
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-23 12:59:54 -0500

    reorder

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     <body>
         <h1>Hello SOFE4630</h1>
 
+        <a href="https://sofe4630a2kdc.herokuapp.com/upload.php">Upload File</a>
 		<a href="https://sofe4630a2kdc.herokuapp.com/list.php">Files List</a>
-		<a href="https://sofe4630a2kdc.herokuapp.com/upload.php">Files List</a>
 
     </body>
 </html>

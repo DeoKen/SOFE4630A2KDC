c1b7bccf2268a5cc779130323da22df923ef1986
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-23 14:26:44 -0500

    foldername

--- upload.php
+++ upload.php
@@ -17,47 +17,46 @@ foreach ($buckets['Buckets'] as $bucket) {
         <h1>Hello SOFE4630</h1>
 
 		<a href="https://sofe4630a2kdc.herokuapp.com/list.php">Files List</a>
-<form action="" method='post' enctype="multipart/form-data">
-<h3>Upload Image</h3><br/>
-<input type='file' name='upload_file'/>
-<input type='submit' name="upload_files" value='Upload'/>
-</form>
 <?php
-if($file_upload_message) {
-echo $file_upload_message;
-}
-?>
-<?php
-include('functions.php');
-$file_upload_message = '';
-if(isset($_POST["upload_files"])) {
-$file_name = $_FILES['upload_file']['name'];
-$file_size = $_FILES['upload_file']['size'];
-$tmp_file = $_FILES['upload_file']['tmp_name'];
-$valid_file_formats = array("jpg", "png", "gif", "bmp","jpeg","PNG","JPG","JPEG","GIF","BMP");
-$file_extension = getFileExtension($file_name);
-if($file_name) {
-if(in_array($file_extension,$valid_file_formats)) {
-if($file_size < (1024*1024)) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
 
-$new_image_name = time().".".$file_extension;
-if($s3->putObjectFile($tmp_file, $bucket , $new_image_name, S3::ACL_PUBLIC_READ)) {
-$file_upload_message = "File Uploaded Successfully to amazon S3.<br><br>";
-$uploaded_file_path='http://'.$bucket.'.s3.amazonaws.com/'.$new_image_name;
-$file_upload_message .= '<b>Upload File URL:</b>'.$uploaded_file_path."<br/>";
-$file_upload_message .= "<img src='$uploaded_file_path'/>";
-} else {
-$file_upload_message = "<br>File upload to amazon s3 failed!. Please try again.";
+	// try upload
+
+	// for now, use existing name
+	$uploadfolder = 'pics';
+	$uploadname = $_FILES['userfile']['name'];
+
+	// set up s3
+	$keyname = $uploadfolder.'/'.$uploadname;
+	// try
+	try {
+	    // Upload data.
+	    $result = $s3->putObject(array(
+	        'Bucket' => $bucket,
+	        'Key'    => $keyname,
+	        'Body'   => fopen($_FILES['userfile']['tmp_name'], 'rb'),
+	        'ACL'    => 'public-read'
+	    ));
+
+	    // Print the URL to the object.
+
+	    // show image
+	    echo('<p><img src="'.$result['ObjectURL'].'" /></p>');
+	    echo('<p>done</p>');
+
+	} catch (S3Exception $e) {
+	    echo $e->getMessage() . "\n";
+	}
+
 }
-} else {
-$file_upload_message = "<br>Maximum allowed image upload size is 1 MB.";
-}
-} else {
-$file_upload_message = "<br>This file format is not allowed, please upload only image file.";
-}
-} else {
-$file_upload_message = "<br>Please select image file to upload.";
-}
-}
+else {
+	?>
+		<h2>Upload a file</h2>
+        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+            <input name="userfile" type="file"><input type="submit" value="Upload">
+        </form>
+
+	<?
+};
 ?>
 </html>

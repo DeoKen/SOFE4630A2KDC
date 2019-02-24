d990c1aa88300c680396387f3e0d77bd51f82539
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-23 22:39:43 -0500

    change to my parameters

--- list.php
+++ list.php
@@ -3,7 +3,7 @@ require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = new Aws\S3\S3Client([
     'version'  => '2006-03-01',
-    'region'   => 'us-east-1',
+    'region'   => 'us-east-2',
 ]);
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
@@ -15,16 +15,16 @@ $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!'
 <?php
 	try {
 		$objects = $s3->getIterator('ListObjects', array(
-			"Bucket" => $bucket
+			"Bucket" => $bucket['Name']
 		));
 		foreach ($objects as $object) {
 ?>
-		<p> <a href="<?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?>"> <?echo $object['Key'] . "<br>";?></a></p>
+		<p> <a href="<?=htmlspecialchars($s3->getObjectUrl($bucket['Name'], $object['Key']))?>"> <?echo $object['Key'] . "<br>";?></a></p>
 		
 <?		}?>
 
 <?php } catch(Exception $e) { ?>
-        <p>error :(</p>
+        echo $e->getMessage() . PHP_EOL;
 <?php }  ?>
     </body>
 </html>

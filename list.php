608831f650bf78d601bbd281e40fcfd0d8d25dcb
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-23 23:45:24 -0500

    hardcoded

--- list.php
+++ list.php
@@ -2,7 +2,7 @@
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = new Aws\S3\S3Client([
-    'version'  => '2012-10-17',
+    'version'  => '2006-03-01',
     'region'   => 'us-east-2',
 ]);
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
@@ -15,6 +15,7 @@ foreach ($buckets['Buckets'] as $bucket) {
     <head><meta charset="UTF-8"></head>
     <body>
 		<h3>S3 Files</h3>
+		<img src="https://s3.us-east-2.amazonaws.com/sofe430a2kdc/merc.jpg">
 
 <?php
 	try {

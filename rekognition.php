7700a73866a18cea460810b6209cdb31e8f51ff3
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-25 00:19:39 -0500

    first try

--- rekognition.php
+++ rekognition.php
@@ -6,6 +6,10 @@ $s3 = new Aws\S3\S3Client([
     'region'   => 'us-east-2',
 ]);
 
+$rekog = new Aws/Rekognition/RekognitionClient([
+    'vertion'   =>  'latest',
+    'region'    => 'us-east-2',
+]);
 
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 $buckets = $s3->listBuckets();
@@ -15,9 +19,26 @@ foreach ($buckets['Buckets'] as $bucket) {
     echo $bucket['Name'] . "\n";
 }
 ?>
+
 <html>
     <head><meta charset="UTF-8"></head>
     <body>
         <h1>Hello SOFE4630</h1>
+        <?php
+            if(isset($_GET['value'])){
+                $key = $_GET['value'];
+            }
+            $result = $rekog->detectLabels([
+                'Image' => [ // REQUIRED
+                    'S3Object' => [
+                        'Bucket' => $bucket['Name'],
+                        'Name' => '$key',
+                    ],
+                ],
+                'MaxLabels' => 10,
+                'MinConfidence' => 20,
+            ]);
+            echo $result['Name']['Confidence'];
+        ?>
     </body>
 </html>

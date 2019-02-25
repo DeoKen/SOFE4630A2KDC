ee62f2f713efdbe1872f212ad054e5ff914c8841
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-24 21:47:15 -0500

    nicer line

--- list.php
+++ list.php
@@ -21,7 +21,8 @@ foreach ($buckets['Buckets'] as $bucket) {
 			"Bucket" => $bucket['Name']
 		));
 		foreach ($objects as $object) {
-		    echo "<img src=https://s3.us-east-2.amazonaws.com/sofe430a2kdc/".$object['Key'] . " height='100' width='100'><br>";
+		    echo "<img src=https://s3.us-east-2.amazonaws.com/sofe430a2kdc/";
+		    echo $object['Key'] . " height='100' width='100'><br><br>";
 
 		}
 

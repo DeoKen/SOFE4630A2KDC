c27aa5f810ed420ef844adbf5b8fe0caf82622d0
Kenneth Dela cruz <100579215@oncampus.local>
2019-02-25 13:35:40 -0500

    new method

--- tts.php
+++ tts.php
@@ -15,7 +15,8 @@
     $file = $file . ".mp3";
     //file_put_contents($file, "");
     $googleAPIKey = 'AIzaSyBFHwK7xiu0O1mlztwcg18yBYPdb-2f0Wk';
-use Google\Cloud\TextToSpeech\V1\AudioConfig;
+    try{
+    use Google\Cloud\TextToSpeech\V1\AudioConfig;
 use Google\Cloud\TextToSpeech\V1\AudioEncoding;
 use Google\Cloud\TextToSpeech\V1\SsmlVoiceGender;
 use Google\Cloud\TextToSpeech\V1\SynthesisInput;
@@ -23,17 +24,21 @@ use Google\Cloud\TextToSpeech\V1\TextToSpeechClient;
 use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
 // instantiates a client
 $client = new TextToSpeechClient();
+echo "new client";
 // sets text to be synthesised
 $synthesis_input = (new SynthesisInput())
     ->setText('Hello, world!');
+    echo "output";
 // build the voice request, select the language code ("en-US") and the ssml
 // voice gender
 $voice = (new VoiceSelectionParams())
     ->setLanguageCode('en-US')
     ->setSsmlGender(SsmlVoiceGender::FEMALE);
+    echo "voice";
 // select the type of audio file you want returned
 $audioConfig = (new AudioConfig())
     ->setAudioEncoding(AudioEncoding::MP3);
+    echo "config";
 // perform text-to-speech request on the text input with selected voice
 // parameters and audio file type
 $response = $client->synthesizeSpeech($synthesis_input, $voice, $audioConfig);
@@ -43,6 +48,9 @@ file_put_contents($file, $audioContent);
     //if (filesize($file)>0){
         echo "<audio controls><source src=".$file." type=audio/mp3></audio>";
     //}
+    }catch (Exception $e){
+        echo $e->getMessage();
+    }
 
 ?>
 <html>
